79860f3137eb1ed635e677f8cd605108b69ddce8
asubiksha27 <asubiksha27@gmail.com>
2026-05-18 11:24:00 +0530

    Add logger middleware for request logging.

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,1 +1,11 @@
+const logger = (req, res, next) => {
+    console.log("=========");
+    console.log(`[LOG] ${new Date().toISOString()}`);
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    console.log(`[IP] ${req.ip}`);
+    console.log("=========");
 
+    next();
+};
+
+module.exports = logger;
